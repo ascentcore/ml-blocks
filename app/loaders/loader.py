1088f94b9e04d69e1b1c202954a844f99ef9a6ac
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-01-22 10:47:56 +0200

    added data loaders and flow

---
+++ app/loaders/loader.py
@@ -0,0 +1,13 @@
+class Loader:
+    def load_files(self, files):
+        raise Exception('not implemented')
+
+    def store(self):
+        raise Exception('not implemented')
+
+    def count(self):
+        pass
+
+
+    def load_page(self, page, count):
+        pass
