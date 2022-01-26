8f3fa8536752d47f9a54265f787786cdd2a83817
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-01-26 17:00:10 +0200

    #7 Modify textfield margin

--- ui/src/components/stories/CustomRegistry/CustomRegistry.styles.js
+++ ui/src/components/stories/CustomRegistry/CustomRegistry.styles.js
@@ -3,7 +3,7 @@ import { makeStyles } from "@mui/styles";
 export const useStyles = makeStyles((theme) => ({
     input: {
         width: '200px',
-        height: '100px',
+        height: '60px',
         marginTop: '20px'
     },
     slider: {
