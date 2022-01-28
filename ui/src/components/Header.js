76dfe98b25847fedf9c693e03080869ed855ab69
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-01-28 15:49:21 +0200

    #9 Add header

--- ui/src/components/Header.js
+++ ui/src/components/Header.js
@@ -1,10 +1,32 @@
+import { AppBar, Container, Grid, Link, Toolbar, Typography } from '@mui/material';
 import React from 'react';
+import { makeStyles } from '@mui/styles';
+
+export const useStyles = makeStyles((theme) => ({
+    appbar: {
+        flexGrow: 1,
+        background: '#6b5b95',
+    },
+    link: {
+        color: '#FFF',
+        marginRight: '10px',
+        marginLeft: '50px'
+    }
+}))
 
 function Header() {
+    const classes = useStyles();
     return (
-        <>
-            <h4>Header</h4>
-        </>
+        <div style={{ flexGrow: 1 }}>
+            <AppBar position='static' className={classes.appbar}>
+                <Container maxWidth="lg">
+                    <Toolbar>
+                        <Link href="/upload" className={classes.link} underline='none'>Upload</Link>
+                        <Link href="/" className={classes.link} underline='none'>Home</Link>
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </div>
     )
 };
 
