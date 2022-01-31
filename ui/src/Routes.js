b242ee50838194835293570cc2fc6366411373b0
ardeleanana <61140469+ardeleanana@users.noreply.github.com>
2022-01-31 12:34:06 +0200

    #11 Edit app and route to navigate to upload

--- ui/src/Routes.js
+++ ui/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import UploadScreen from './screens/UploadScreen';
 
 function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={HomeScreen} />
-            <Route path="/upload" />
+            <Route exact path="/upload" component={UploadScreen} />
         </Switch>
     )
 }
