d5543299918218e2fa49202f971544983d60902a
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-02-04 09:26:40 +0200

    removed UI since was moved to its own repo

--- ui/src/api/API.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const API_BASE = 'http://localhost/api/v1';
-
-export default axios.create({
-    baseURL: API_BASE,
-    responseType: "json"
-});
--- ui/src/api/data.js
+++
@@ -1,9 +0,0 @@
-import API from './API';
-
-export const dataCount = async () => {
-    return await API.get('/data/count');
-}
-
-export const getData = async () => {
-    return await API.get('/data/');
-}
