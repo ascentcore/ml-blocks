303ee4d1e7ac82270bd43d8cb225a8475ebf040a
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-03-04 12:52:14 +0200

    added cleanup

--- app/main.py
+++ app/main.py
@@ -46,6 +46,7 @@ async def startup():
 
     global initialized
     if not initialized:
+        cleanup(db)
         set_status(db, 'pending')
         logger.info('ML-Blocks startup complete...')
     
