828122c28149fc10ca5aa1e7b6b1db35af251781
Cornel Stefanache <cornel.stefanache@gmail.com>
2022-04-13 14:55:01 +0300

    added missing update

--- samples/load-pipeline/block.py
+++ samples/load-pipeline/block.py
@@ -13,4 +13,8 @@ class Block:
     loaders = ['file_loader', 'pandas_loader']
 
 
-
+    def process_data(self, loader, data):
+        name = loader.__class__.__name__
+        if name == 'PandasLoader':
+            return data[:5]
+        return data
