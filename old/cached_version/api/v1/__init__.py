72d7ac0044d2a5565c9c822541543b036967fa10
christianRaita <cristian_cjr@live.com>
2023-01-17 10:01:55 +0200

    Cleanup workarea

--- old/cached_version/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from . import data
-from . import pipeline
-
-api_router = APIRouter()
-
-api_router.include_router(data.router, prefix="/data", tags=["data"])
-api_router.include_router(pipeline.router, prefix="/pipeline", tags=["pipeline"])
--- old/old-app/custom/block.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Block:
-    name = os.getenv("BLOCK_NAME", "Pass Through ML Block")
